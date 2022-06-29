a623e40a01614baa0fd2ed06c6197129313b17aa
highright96 <nsw5296@naver.com>
2022-06-29 14:51:36 +0900

    init: 프로젝트 세팅

---
+++ spring-webflux-todo-with-kotlin/src/main/kotlin/com/highright/springwebfluxtodowithkotlin/SpringWebfluxTodoWithKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.highright.springwebfluxtodowithkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringWebfluxTodoWithKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringWebfluxTodoWithKotlinApplication>(*args)
+}
